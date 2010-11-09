2de52652a438149853719a3f04230b198b28c31a
Silviu Grijincu <silviu.grijincu@gmail.com>
2010-11-09 14:01:17 +0200

    Critters are polygons now too

--- Critter.java
+++ Critter.java
@@ -5,15 +5,10 @@ public class Critter {
 	int WIDTH = 40, HEIGHT=40;
 	int x = 50;
 	int y = 50;
-
-	Point TOP_LEFT_EDGE  = new Point();
-	Point TOP_RIGHT_EDGE = new Point();
-	Point LOW_LEFT_EDGE  = new Point();
-	Point LOW_RIGHT_EDGE = new Point();
-
+	
 	int keep_direction = 0;
-	int direction = EAST;
-
+	int direction = NORTH;
+	
 	static final int NORTH = 1;
 	static final int EAST  = 2;
 	static final int SOUTH = 3;
@@ -22,131 +17,97 @@ public class Critter {
 	static final int NWEST = 6;
 	static final int SEAST = 7;
 	static final int SWEST = 8;
-
+	
 	Random rand = new Random();
-
-	Point p1, p2 ,p3, p4;
-	double angle = 45;
-
+	
+	boolean growing = true;
+	int pase = 2;
+	
+	Polygon poli = new Polygon();
+	
 	public Critter(int init_x, int init_y) {
-		x = init_x;
-		y = init_y;
+		this.x = init_x;
+		this.y = init_y;
+		poli.addPoint(0, 0);
+		poli.addPoint(0, WIDTH);
+		poli.addPoint(WIDTH, HEIGHT);
+		poli.addPoint(HEIGHT, 0);
 	}
-
+	
 	public void paint(Graphics g) {
-
-		g.setColor(Color.black);
-		g.drawRect(x + Volfied.GRID_X, y + Volfied.GRID_Y, WIDTH, HEIGHT);
-		g.setColor(Color.orange);
-		g.fillRect(x + 1 + Volfied.GRID_X, y + Volfied.GRID_Y + 1, WIDTH-1, HEIGHT-1);
+		g.setColor(Color.ORANGE);
+		g.fillPolygon(this.getPaintable());
 	}
-
-
+	
+	
 	public void draw(Graphics g_main){
 		animate();
 		this.paint(g_main);
 	}
-
-
+	
 	public boolean isDead() {
 		//TODO: FIXME: implement me
 		if (!Volfied.terain.poli.toPolygon().contains(this.x, this.y))
 			return true;
 		return false;
 	}
-
+	
+	public Polygon getPaintable() {
+		Polygon ret = new Polygon(poli.xpoints, poli.ypoints, poli.npoints);
+		ret.translate(x + Volfied.GRID_X, y + Volfied.GRID_Y);
+		return ret;
+	}
+	
+	public int getPosition(BrokenLine[] polys) {
+		
+		if (polys[0].toPolygon().contains(this.x, this.y))
+			return 0;
+		if (polys[1].toPolygon().contains(this.x, this.y))
+			return 1;
+		return 1;
+	}
+	
+	
 	public void generateDirection() {
 		direction = rand.nextInt(SWEST - NORTH + 1) + NORTH;
 	}
-
+	
+	
+	public boolean canGrow() {
+		if (WIDTH == 120 || HEIGHT == 120) {
+			growing = false;
+		}
+		
+		if (WIDTH == 75 || HEIGHT == 75) {
+			growing = true;
+		}
+		return false;
+	}
+	
+	public boolean isOuter(Point nex_ship_pos) {
+		int n = this.poli.npoints;
+		for (int i = 0; i < n; i++)
+			if (Volfied.terain.isOuter(new Point(poli.xpoints[i] + nex_ship_pos.x, poli.ypoints[i] + nex_ship_pos.y)))
+				return true;
+		return false;
+	}
+	
 	public void animate() {
-		//System.out.println("LEFT_EDGE = " + TOP_LEFT_EDGE.x + ", " + TOP_LEFT_EDGE.y);
-		if (keep_direction == 105) {
+		if (keep_direction == 400) {
 			generateDirection();
 			keep_direction = 0;
 		}
-		keep_direction++;
-		switch (direction) {
-		case NORTH:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x + WIDTH/2, TOP_LEFT_EDGE.y + HEIGHT/2- 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(TOP_RIGHT_EDGE.x + WIDTH/2, TOP_RIGHT_EDGE.y + HEIGHT/2 - 1)))
-				y--;
-			else generateDirection();
-			break;
-
-		case SOUTH:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(LOW_LEFT_EDGE.x+ WIDTH/2, LOW_LEFT_EDGE.y + WIDTH/2+ 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x+ WIDTH/2, LOW_RIGHT_EDGE.y + WIDTH/2+ 1)))
-				y++;
-			else generateDirection();
-			break;
-
-		case EAST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x+ WIDTH/2 + 1, TOP_LEFT_EDGE.y+ WIDTH/2)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x + WIDTH/2+ 1, LOW_RIGHT_EDGE.y+ WIDTH/2)))
-				x++;
-			else generateDirection();
-			break;
-
-		case WEST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x + WIDTH/2- 1, TOP_LEFT_EDGE.y+ WIDTH/2)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x+ WIDTH/2 - 1, LOW_RIGHT_EDGE.y+ WIDTH/2)))
-				x--;
-			else generateDirection();
-			break;
-
-		case NEAST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(TOP_RIGHT_EDGE.x+ WIDTH/2 + 1, TOP_RIGHT_EDGE.y+ WIDTH/2 - 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x+ WIDTH/2 + 1, TOP_LEFT_EDGE.y + WIDTH/2- 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x + WIDTH/2+ 1, LOW_RIGHT_EDGE.y+ WIDTH/2 - 1))) {
-				x++;
-				y--;
-			}
-			else generateDirection();
-			break;
-
-		case NWEST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x + WIDTH/2- 1, TOP_LEFT_EDGE.y + WIDTH/2- 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x + WIDTH/2- 1, LOW_RIGHT_EDGE.y + WIDTH/2- 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(TOP_RIGHT_EDGE.x+ WIDTH/2 - 1, TOP_RIGHT_EDGE.y + WIDTH/2- 1))) {
-				x--;
-				y--;
-			}
-			else generateDirection();
-			break;
-
-		case SEAST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x + WIDTH/2+ 1, LOW_RIGHT_EDGE.y+ WIDTH/2 + 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(TOP_RIGHT_EDGE.x+ WIDTH/2 + 1, TOP_RIGHT_EDGE.y+ WIDTH/2 + 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_LEFT_EDGE.x + WIDTH/2+ 1, LOW_LEFT_EDGE.y + WIDTH/2+ 1))) {
-				y++;
-				x++;
-			}
-			else generateDirection();
-			break;
-
-		case SWEST:
-			if (!Volfied.terain.isPointOnPerimeter(new Point(LOW_LEFT_EDGE.x + WIDTH/2- 1, LOW_LEFT_EDGE.y + WIDTH/2+ 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(TOP_LEFT_EDGE.x + WIDTH/2- 1, TOP_LEFT_EDGE.y + WIDTH/2+ 1)) &&
-					!Volfied.terain.isPointOnPerimeter(new Point(LOW_RIGHT_EDGE.x + WIDTH/2- 1, LOW_RIGHT_EDGE.y + WIDTH/2+ 1))) {
-				y++;
-				x--;
-			}
-			else generateDirection();
-			break;
-
-		default:
+		Point curr_ship_pos = new Point(this.x, this.y);
+		Point nex_shipp_pos = curr_ship_pos.getShipNewPosition(direction, pase);
+		
+		if(isOuter(nex_shipp_pos))
 			generateDirection();
-			break;
+		else {
+			this.x = nex_shipp_pos.x;
+			this.y = nex_shipp_pos.y;
 		}
-
-		TOP_LEFT_EDGE.x  = x - WIDTH/2;
-		TOP_LEFT_EDGE.y  = y - HEIGHT/2;
-		TOP_RIGHT_EDGE.x = x + WIDTH/2;
-		TOP_RIGHT_EDGE.y = y - HEIGHT/2;
-		LOW_LEFT_EDGE.x  = x - WIDTH/2;
-		LOW_LEFT_EDGE.y  = y + HEIGHT/2;
-		LOW_RIGHT_EDGE.x = x + WIDTH/2;
-		LOW_RIGHT_EDGE.y = y + HEIGHT/2;
+			
+		keep_direction++;
+		
 	}
 }
